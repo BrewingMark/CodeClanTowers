08239f7a0eb6f446205a37959b2cdbdcb1aa9762
Mark Nicol <marknicol1991@live.co.uk>
2020-05-27 15:55:26 +0100

    Comments out not working method.

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -43,7 +43,7 @@ public class Hotel {
         this.diningRooms.put(diningRoom.getName(), diningRoom);
     }
 
-    public ArrayList<Room> getEmptyRooms() {
-        ArrayList<Room> emptyRooms = this.rooms.removeIf();
-    }
+//    public ArrayList<Room> getEmptyRooms() {
+//        ArrayList<Room> emptyRooms = this.rooms.removeIf();
+//    }
 }
